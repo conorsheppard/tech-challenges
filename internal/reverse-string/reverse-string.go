53c276d4f8301636859b643fd8b4b5894e4e7445
David Belka <david.belka@divido.com>
2022-05-10 14:30:50 +0100

    feat: wrote a function to reverse a string which respects UTF8 encoding and combined characters

--- internal/reverse-string/reverse-string.go
+++ internal/reverse-string/reverse-string.go
@@ -1,6 +1,46 @@
 package reverse_string
 
-// Add your solution here
+import (
+	"unicode"
+)
+
+// Main function
 func reverseString(s string) string {
-	return ""
+	textRunes := []rune(s)
+	textRunesLength := len(textRunes)
+	if textRunesLength <= 1 {
+		return s
+	}
+
+	i, j := 0, 0
+	for i < textRunesLength && j < textRunesLength {
+		j = i + 1
+		for j < textRunesLength && isMark(textRunes[j]) {
+			j++
+		}
+
+		if isMark(textRunes[j-1]) {
+			// Reverses Combined Characters
+			reverse(textRunes[i:j], j-i)
+		}
+
+		i = j
+	}
+
+	// Reverses the entire array
+	reverse(textRunes, textRunesLength)
+
+	return string(textRunes)
+}
+
+// Reverse helping function
+func reverse(runes []rune, length int) {
+	for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+}
+
+// isMark determines whether the rune is a marker
+func isMark(r rune) bool {
+	return unicode.Is(unicode.Mn, r) || unicode.Is(unicode.Me, r) || unicode.Is(unicode.Mc, r)
 }
