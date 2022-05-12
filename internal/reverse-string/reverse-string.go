719e949cd0631398e73b77867fe5b40cf1f81d3d
Chris Campbell <97600504+campbellcj-divido@users.noreply.github.com>
2022-05-12 20:19:48 +0100

    Update reverse-string.go

--- internal/reverse-string/reverse-string.go
+++ internal/reverse-string/reverse-string.go
@@ -1,6 +1,32 @@
 package reverse_string
 
 // Add your solution here
+
+// Function to reverse string
 func reverseString(s string) string {
-	return ""
+
+	// Convert string to rune array
+	r := []rune(s)
+
+	// Take length of rune array with len
+	length := len(r)
+
+	// Create reversed string
+	rev := ""
+
+	i := length
+	for i >= 1 {
+
+		rev = rev + string(r[i-1])
+		i = i - 1
+	}
+
+	//Return reversed string
+	return rev
+
 }
+
+
+
+
+
