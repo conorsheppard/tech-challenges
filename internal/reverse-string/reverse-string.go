fd2006db5998f7dcf585bfde89b7dff4f3f1af01
David Thai <7222115+dafuloth@users.noreply.github.com>
2022-05-13 12:37:36 +0100

    TC-001 reverse string: winner (#4)

--- internal/reverse-string/reverse-string.go
+++ internal/reverse-string/reverse-string.go
@@ -1,6 +1,15 @@
 package reverse_string
 
-// Add your solution here
 func reverseString(s string) string {
-	return ""
+	// string to rune slice
+	runes := []rune(s)
+
+	var result []rune
+
+	// iterate rune slice in reverse, build result
+	for i := len(runes) - 1; i >= 0; i-- {
+		result = append(result, runes[i])
+	}
+
+	return string(result)
 }
